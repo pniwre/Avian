62232a170492b3440f4b148b989b1f5d0af4fbc9
alamshafil <36016500+alamshafil@users.noreply.github.com>
2021-11-21 15:46:38 -0500

    Change to avian server

--- src/ravend.cpp
+++ src/ravend.cpp
@@ -152,7 +152,7 @@ bool AppInit(int argc, char* argv[])
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #endif
-            fprintf(stdout, "Raven server starting\n");
+            fprintf(stdout, "Avian server starting\n");
 
             // Daemonize
             if (daemon(1, 0)) { // don't chdir (1), do close FDs (0)
